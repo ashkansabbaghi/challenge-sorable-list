deb58e5176915d16ecf05c042a4b92e3f7260dc1
ashkan73 <ashkansabbaghi73@gmail.com>
2024-01-15 16:06:33 +0330

    debug select skill

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex flex-col items-center justify-center w-full h-full p-5">
-        <h1 class="md:text-3xl text-xl font-bold text-gray-800 dark:text-gray-200 ">Select Your  Top 5 Tech Skills</h1>
+        <h1 class="md:text-3xl text-xl font-bold text-gray-800 dark:text-gray-200 ">Select Your Top 5 Tech Skills</h1>
         <div class="grid m-8 border border-gray-200 rounded-lg shadow-sm md:w-full max-w-[900px]
             dark:border-gray-700 md:mb-12 md:grid-cols-2 bg-white dark:bg-gray-800">
             <figure class="flex flex-col gap-y-5 items-center justify-center p-8 text-center bg-white border-b border-gray-200 rounded-t-lg
@@ -11,15 +11,16 @@
                         dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white 
                         dark:focus:ring-blue-500 dark:focus:border-blue-500 disabled:opacity-30"
                         :placeholder="`skill ${iNum + 1}`" :disabled="checkStepDisable(num)" autocomplete="off"
-                        @keyup.enter.prevent="setSelectedSkill()" @keydown.tab="isSuggestionSkill.fill(false)" @click.prevent="selectInput(num)"
+                        @keyup.enter.prevent="setSelectedSkill()"
+                        @keydown.tab.prevent="setSuggestionSkill(skillsModel[num])" @click.prevent="selectInput(num)"
                         @input="openSuggestionDropDown(iNum)">
                     <XMarkIcon class="w-4 absolute top-[30%] right-3 dark:stroke-white cursor-pointer"
                         @click.prevent="skillsModel[num] = ''" />
                     <div v-if="isSuggestionSkill[iNum]" class="z-10 shadow w-full absolute right-0 h-[15rem] overflow-auto">
                         <ul>
-                            <li v-for="(skill, iSkill) in filtersSuggestion" :key="iSkill" class="cursor-pointer bg-white p-2 text-black border-b border-slate-200 
-                                dark:bg-slate-600 dark:text-slate-200 dark:border-slate-700 hover:bg-gray-100 dark:hover:bg-slate-700"
-                                @click.prevent="setSuggestSideSkill(skill)">
+                            <li v-for="(skill, iSkill) in filtersSuggestion" :key="iSkill"
+                                class="cursor-pointer bg-white p-2 text-black border-b border-slate-200 
+                                dark:bg-slate-600 dark:text-slate-200 dark:border-slate-700 hover:bg-gray-100 dark:hover:bg-slate-700" @click.prevent="setSuggestionSkill(skill)">
                                 <p class="ms-2 text-sm font-medium cursor-pointer [&>*]:cursor-pointer">
                                     <span class="text-gray-500 dark:text-slate-100 text-xs"> {{ skill }}</span>
                                 </p>
@@ -35,7 +36,7 @@
                 <ul v-for="(skill, iSkill) in suggestSkills" :key="iSkill"
                     class="max-w-md space-y-1 text-gray-500 list-disc list-inside dark:text-gray-400">
                     <li>
-                        <span class="cursor-pointer" @click.prevent="setSuggestSideSkill(skill)">{{ skill }}</span>
+                        <span class="cursor-pointer" @click.prevent="setSuggestionSkill(skill)">{{ skill }}</span>
                     </li>
                 </ul>
 
@@ -73,11 +74,13 @@ const selectInput = (num) => {
     currInput.value = num
 }
 
-const setSuggestSideSkill = (skill) => {
+
+const setSuggestionSkill = (skill) => {
     if (!currInput.value) return
     skillsModel.value[currInput.value] = skill
     const targetInput = refInput.value[currInput.value]
-    if (targetInput) targetInput.focus()
+    if (targetInput) targetInput.disabled = false
+    targetInput?.focus()
     currInput.value++
     isSuggestionSkill.value.fill(false)
 }
